3b53e6f4584f2186f13e2420132c262ba1a5a4dd
Jrains-ux <huktand9@gmail.com>
2025-08-28 12:11:13 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export let PROXY_SECRET = 'Easy-Reverse-Proxy'; // 代理密钥
 // 代理
 export let PROXIES: Proxy[] = [
   {
-    domain:"https://easy-reverse-proxy-g1qan5ysq-jrains-projects.vercel.app/",
+    domain:"easy-reverse-proxy-bc8e0xjr9-jrains-projects.vercel.app",
     url:"https://www.google.com/"
   }
 ];
