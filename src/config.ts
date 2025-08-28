ceadc9b5bc23a43b6481d83fbebd2b16d09cbe5c
Jrains-ux <huktand9@gmail.com>
2025-08-28 11:57:14 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,9 @@ export let PORT = 377; // 端口
 
 export let PROXY_SECRET = 'Easy-Reverse-Proxy'; // 代理密钥
 // 代理
-export let PROXIES: Proxy[] = [];
+export let PROXIES: Proxy[] = [
+  {
+    domain:"easy-reverse-proxy.vercel.app",
+    url:"https://www.google.com/"
+  }
+];
